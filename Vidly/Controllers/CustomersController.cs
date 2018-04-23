f390e8c4375600c8261d0aa3b5c9bd9db1ee9da7
Wesley Reuel <wreuel@gmail.com>
2018-04-23 00:27:47 -0300

    Primeiro Commit do curso

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            //var customers = new List<Customer>
+            //{
+            //    new Customer{Name = "John Smith", Id = 1 },
+            //    new Customer{Name = "Marie", Id =2 }
+            //};
+
+            var customers = GetCustomers();
+            //retorno.Customers = customers;
+
+            return View(customers);
+
+
+        }
+
+        //[Route("movies/released/{year:regex(\\d{2})}/{month:regex(\\d{2}):range(1, 12)}")]
+        [Route("Customers/Details/{id:regex(\\d)}")]
+        public ActionResult Details(int id)
+        {
+            /*var customer = new Customer();
+            if (id == 1)
+            {
+                customer.Name = "John Smith";
+            }
+            else if (id == 2)
+            {
+                customer.Name = "Marie";
+            }
+            else
+            {
+                customer.Name = "Cliente não existe";
+            }
+            return View(customer);*/
+
+            var customer = GetCustomers().SingleOrDefault(s => s.Id == id);
+
+            if(customer == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(customer);
+            }
+        }
+
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith" },
+                new Customer { Id = 2, Name = "Mary Williams" }
+            };
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{ 
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer{Name = "Customer 1" },
+                new Customer{Name = "Customer 2" }
+            };
+            //ViewData["Movie"] = movie;
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+            //return View(movie);
+            //return Content("Hello");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        public ActionResult Index()
+        {
+            List<Movie> movies = new List<Movie>();
+            movies.Add(new Movie() { Name = "Sherk", Id = 1 });
+            movies.Add(new Movie() { Name = "Wall-e", Id = 2 });
+
+
+            return View(movies);
+        }
+
+
+
+        [Route("movies/released/{year:regex(\\d{2})}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, byte month)
+        {
+            return Content(year+"/"+month);
+        }
+        
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // movies
+        /*public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return (Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy)));
+        }*/
+    }
+}
