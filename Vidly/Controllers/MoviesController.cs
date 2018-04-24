93d389210a10e9e92c03e90de22d66331bc6abab
Wesley Reuel <wreuel@gmail.com>
2018-04-24 15:53:32 -0300

    Editando filmes

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -23,6 +23,46 @@ namespace Vidly.Controllers
             _context.Dispose();
         }
 
+
+
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+
+            MovieFormViewModel viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.GenreId = movie.GenreId;
+               // movieInDb.DateAdded = DateTime.Now;
+            }
+
+            _context.SaveChanges();
+
+
+            return RedirectToAction("Index", "Movies");
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
@@ -83,7 +123,20 @@ namespace Vidly.Controllers
 
         public ActionResult Edit(int id)
         {
-            return Content("id=" + id);
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
         }
     }
 }
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -10,14 +10,22 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
         [Required]
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+
         [Required]
         public DateTime DateAdded { get; set; }
+
         [Required]
+        [Display(Name="Numer in Stock")]
         public int NumberInStock { get; set; }
-        [Required]
+
+        
         public Genre Genre { get; set; }
+
+        [Required]
         public byte GenreId { get; set; }
     }
 }
