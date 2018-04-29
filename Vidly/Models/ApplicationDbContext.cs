9f9640280519dd04d97c51bd5fb7499519bedec9
Wesley Reuel <wreuel@gmail.com>
2018-04-29 17:57:36 -0300

    Fim da sessao 8

--- Vidly/Models/ApplicationDbContext.cs
+++ Vidly/Models/ApplicationDbContext.cs
@@ -1,8 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Data.Entity;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
+﻿using System.Data.Entity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Vidly.Models
